b83004916f03b07acec7fc3c0478fcef8632bd61
Taras <taras.zadziarnouski@gmail.com>
2020-08-20 15:02:42 +0300

    Lesson2

---
+++ src/by/zadziarnouski/Lesson2/Lesson2_7.java
@@ -0,0 +1,21 @@
+package by.zadziarnouski.Lesson2;
+
+import java.util.Scanner;
+
+/**
+ * В переменную записываете количество программистов. В зависимости от количества
+ * программистов необходимо вывести правильно окончание. Например: • 2 программиста
+ * • 1 программиста • 10 программистов • и т.д.
+ **/
+
+public class Lesson2_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int countOfProgrammer = sc.nextInt();
+        if (countOfProgrammer == 1) {
+            System.out.println(countOfProgrammer + " программист");
+        } else if (2 <= countOfProgrammer && countOfProgrammer <= 4) {
+            System.out.println(countOfProgrammer + " программиста");
+        } else System.out.println(countOfProgrammer + " программистов");
+    }
+}
