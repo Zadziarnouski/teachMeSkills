29b9087ef87e264d18d75198ca4d4519fa1fc601
Taras <taras.zadziarnouski@gmail.com>
2020-08-20 16:02:39 +0300

    Lesson4

---
+++ src/Lesson4/Task18.java
@@ -0,0 +1,24 @@
+/*
+ * Задание 18
+ * Найти в строке не только запятые, но и другие знаки препинания. Подсчитать
+ * общее их количество.
+ */
+
+package Lesson4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task18 {
+    public static void main(String[] args) {
+        String str = "Строковый литерал - это последова:тель-ность символов, заключенных в двойные кавычки.";
+        int counter = 0;
+        String regex = "[\\p{Punct}]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            counter++;
+        }
+        System.out.println(String.format("В этом предложении %d знаков припинания или др.символов", counter));
+    }
+}
